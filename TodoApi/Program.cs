7a3f4816ea5aae6b88a068087e10d78303285047
nakaoshougo <knpy0508@gmail.com>
2023-10-20 23:18:27 +0900

    test

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -24,3 +24,12 @@ app.MapControllers();
 
 app.Run();
 
+[HttpPost]
+public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+{
+    _context.TodoItems.Add(todoItem);
+    await _context.SaveChangesAsync();
+
+    //    return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+    return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+}
