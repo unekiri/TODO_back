1da5d142db683db8a4eba55dd800c61a345e9ee0
nakaoshougo <knpy0508@gmail.com>
2023-11-11 14:30:17 +0900

    モデルに対してバリデーションを設定しました。(文字数制限と日付記入の必須化)

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -1,9 +1,16 @@
 ﻿namespace TodoApi.Models;
+using System.ComponentModel.DataAnnotations;
+
 
 public class TodoItem
 {
     public long Id { get; set; }
+
+    [StringLength(100)]
     public string? Name { get; set; }
+
     public bool IsComplete { get; set; }
+
+    [Required(ErrorMessage = "日付を入力して下さい。")]
     public DateTime Date { get; set; }
 }
