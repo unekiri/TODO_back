3569dd81fb4f5cfe7025e9b256e79ee0ba55062e
nakaoshougo <knpy0508@gmail.com>
2023-11-15 21:28:53 +0900

    モデルにもバリデーションを記述しました

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -1,13 +1,20 @@
 ﻿// エンティティ(DBのテーブルに対応する)
 
 namespace TodoApi.Models;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 
 public class TodoItem
 {
     public long Id { get; set; }
+
+    [Required(ErrorMessage = "TODOを入力して下さい")]
+    [StringLength(100, ErrorMessage = "入力は100文字以内にして下さい")]
     public string? Name { get; set; }
+
+    [Required(ErrorMessage = "日付を入力して下さい")]
     public bool IsComplete { get; set; }
+
     public DateTime Date { get; set; }
 }
